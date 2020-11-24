8b7a368345836d1dbd95aa484d5635ac92a753d3
sujitsharma1166 <sujit.dhakal1166@gmail.com>
2020-11-24 07:23:51 +0545

    Initial commit

---
+++ src/test/java/com/infodevelopers/basic_authentication/BasicAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infodevelopers.basic_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
